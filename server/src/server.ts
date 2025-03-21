ad3f713a047fe097909048127d4d8710775fc65d
Mikey Palermo <mikeypalermo@Mikeys-MacBook-Air.local>
2025-03-21 11:52:31 -0400

    MVP Phase

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,29 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
 import { YoutubeTranscript } from 'youtube-transcript';
+import OpenAI from 'openai';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+const openAi = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const responseGenerate = async (prompt: string) => {
+    const completion = await openAi.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {"role": "system", "content": "You will summarize the transcript of a youtube video provided by the user. You are professional and informative."},
+        {"role": "user", "content": `Write a half-page summary that is specific and encompasses all important events for the following youtube video transcript: "${prompt}"`}
+      ]
+    });
+
+    return completion.choices[0].message.content;
+};
 
 
 app.post("/api", async (req: Request, res: Response) => {
@@ -14,7 +33,22 @@ app.post("/api", async (req: Request, res: Response) => {
 
   if (url) {
     const transcript = await YoutubeTranscript.fetchTranscript(url,{ lang: 'en' });
-    res.status(200).json({ transcript });
+
+    const cleanedTexts = transcript.map((entry: { text: string }) => {
+        return entry.text
+        .replace(/\/n/g, " ")
+        .replace(/\s+/g, " ")
+        .trim();
+    });
+
+    const joinedText = cleanedTexts.join(" ");
+
+    const aiOutput  = await responseGenerate(joinedText)
+
+    res.status(201).json({
+        message: "AI output created successfully.",
+        aiOutput,
+      });
   } else {
     res.status(400).json({ message: "No URL provided" });
   }
