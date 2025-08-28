1f203ca3f971dabfc279a85efb90c35c9c388506
Michael Palermo <mikeypalermo7@gmail.com>
2025-08-28 17:49:09 -0400

    fixing deployment issues

--- server/src/server.ts
+++ server/src/server.ts
@@ -12,6 +12,7 @@ app.use(express.json());
 // mount routes
 app.use("/api", transcriptRoutes);
 
-app.listen(5001, () => {
-  console.log("Server running on http://localhost:5001");
+const port = Number(process.env.PORT) || 5001;
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server running on port ${port}`);
 });
