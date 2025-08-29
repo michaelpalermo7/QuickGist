33aa6ec2ddd472fc292889fb0ee71df9e18d2f58
Michael Palermo <mikeypalermo7@gmail.com>
2025-08-29 11:30:27 -0400

    logo leads to home logic

--- client/src/components/Logo.tsx
+++ client/src/components/Logo.tsx
@@ -4,7 +4,7 @@ import logo from "../assets/logo333.png";
 export const Logo = () => {
   return (
     <div className="pt-2 -m-20">
-      <a href="/home">
+      <a href="/">
         <img src={logo} alt="Logo" className="w-50 mx-auto" />
       </a>
     </div>
